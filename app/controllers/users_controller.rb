10454ddb49582d16cca57f1fcdc5b5beb2419e10
Matt Burton <madeofburtonium@gmail.com>
2016-04-30 19:23:47 +0000

    updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,19 @@ class UsersController < ApplicationController
     end
   end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # update the fucker
+    else
+      render 'edit'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   private
   
   def user_params
