e1ded28aa0e95b85d18f85f8648a8171a589a512
Matt Burton <madeofburtonium@gmail.com>
2016-05-09 02:42:30 +0000

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@ User.create!(name:  "Matt Burton",
              language:              "klingon",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true, 
+             activated: true,
+             activated_at: Time.zone.now)
              
 99.times do |n|
   name  = Faker::Name.name
@@ -18,5 +20,7 @@ User.create!(name:  "Matt Burton",
                email: email,
                language: language,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password, 
+               activated: true,
+               activated_at: Time.zone.now)
 end
